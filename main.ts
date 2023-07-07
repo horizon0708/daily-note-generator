cb5c6ee006dc40626e726bce6362ddb41f4caaa2
James Kim <james@sidenotes.xyz>
2023-07-07 11:01:59 +0900

    open next and previous daily note

--- main.ts
+++ main.ts
@@ -8,6 +8,8 @@ import {
 	PluginSettingTab,
 	Setting,
 } from "obsidian";
+import { DailyNoteGenerator } from "src/daily-note-generator";
+import { DailyNoteNavigator } from "src/daily-note-navigator";
 
 // Remember to rename these classes and interfaces!
 
@@ -43,12 +45,40 @@ export default class MyPlugin extends Plugin {
 
 		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: "open-sample-modal-simple",
-			name: "Open sample modal (simple)",
-			callback: () => {
-				new SampleModal(this.app).open();
+			id: "generate-daily-notes",
+			name: "Generate Daily Notes",
+			callback: async () => {
+				const genrator = new DailyNoteGenerator(this.app, 2023, 7, {
+					folder: "Daily",
+				});
+				await genrator.generateNotes();
 			},
 		});
+
+		this.addCommand({
+			id: "open-next-daily-note",
+			name: "Next Daily Note",
+			callback: async () => {
+				const path = this.app.workspace.activeEditor?.file?.path;
+				const nav = new DailyNoteNavigator(this.app, {
+					folder: "Daily",
+				});
+				nav.openOffsetDays(path, 1);
+			},
+		});
+
+		this.addCommand({
+			id: "open-previous-daily-note",
+			name: "Previous Daily Note",
+			callback: async () => {
+				const path = this.app.workspace.activeEditor?.file?.path;
+				const nav = new DailyNoteNavigator(this.app, {
+					folder: "Daily",
+				});
+				nav.openOffsetDays(path, -1);
+			},
+		});
+
 		// This adds an editor command that can perform some operation on the current editor instance
 		this.addCommand({
 			id: "sample-editor-command",
@@ -56,6 +86,7 @@ export default class MyPlugin extends Plugin {
 			editorCallback: (editor: Editor, view: MarkdownView) => {
 				console.log(editor.getSelection());
 				editor.replaceSelection("Sample Editor Command");
+
 				new Notice(`${window.moment.now()}`);
 			},
 		});
@@ -82,17 +113,6 @@ export default class MyPlugin extends Plugin {
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, "click", (evt: MouseEvent) => {
-			console.log("click", evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(
-			window.setInterval(() => console.log("setInterval"), 5 * 60 * 1000)
-		);
 	}
 
 	onunload() {}
